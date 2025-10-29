cc2295d32565430fe14f7414b2a9ae901c271f10
filip <filip1folga@gmail.com>
2025-10-29 13:23:39 +0100

    backend and frontend logic for textshuffle app

---
+++ textshuffle/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class UploadTextForm(forms.Form):
+    file = forms.FileField(label="Wgraj plik tekstowy")
+
+    def clean_file(self):
+        uploaded_file = self.cleaned_data['file']
+        try:
+            content = uploaded_file.read().decode('utf-8')
+        except UnicodeDecodeError:
+            raise forms.ValidationError(
+                "Nieprawidłowy format pliku. Proszę wgrać plik tekstowy w kodowaniu UTF-8."
+            )
+        # zapisujemy w cleaned_data zdekodowany tekst
+        self.cleaned_data['text_content'] = content
+        return uploaded_file
--- textshuffle/views.py
+++ textshuffle/views.py
@@ -1,4 +1,45 @@
+import random
+import re
+
 from django.shortcuts import render
 
+from textshuffle.forms import UploadTextForm
+
+
 def dashboard(request):
-    return render(request, "textshuffle/dashboard.html")
+    text_to_display = ""
+    is_shuffled = False
+    header = ""
+
+    if request.method == 'POST':
+        if 'upload' in request.POST:
+            form = UploadTextForm(request.POST, request.FILES)
+            if form.is_valid():
+                text_to_display = form.cleaned_data['text_content']
+                header = "Oryginalny tekst"
+        elif 'shuffle' in request.POST:
+            form = UploadTextForm()
+            text_to_display = request.POST.get('text_to_display', '')
+            if text_to_display:
+                text_to_display = shuffle_text(text_to_display)
+                is_shuffled = True
+                header = "Zamieszany tekst"
+    else:
+        form = UploadTextForm()
+
+    return render(request, 'textshuffle/dashboard.html', {
+        'text_to_display': text_to_display,
+        'is_shuffled': is_shuffled,
+        'header': header,
+        'form': form
+    })
+
+def shuffle_text(text):
+    def shuffle_word(word):
+        if len(word) <= 3:
+            return word
+        middle = list(word[1:-1])
+        random.shuffle(middle)
+        return word[0] + "".join(middle) + word[-1]
+
+    return re.sub(r'\w+', lambda m: shuffle_word(m.group()), text)
