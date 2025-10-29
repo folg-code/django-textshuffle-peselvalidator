3e991a0ffb883f2d1ccc2ce3f81faded8a51344a
filip <filip1folga@gmail.com>
2025-10-29 15:04:42 +0100

    backend and frontend logic for peselvalidator app

--- peselvalidator/views.py
+++ peselvalidator/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
+from peselvalidator.forms import PESELForm
+
+
 def dashboard(request):
-    return render(request, "peselvalidator/dashboard.html")
+    result = None
+
+    if request.method == 'POST':
+        form = PESELForm(request.POST)
+        if form.is_valid():
+            result = {
+                'is_valid': True,
+                'birth_date': form.cleaned_data['birth_date'],
+                'gender': form.cleaned_data['gender']
+            }
+        else:
+            result = {'is_valid': False, 'error': form.errors['pesel'][0]}
+    else:
+        form = PESELForm()
+
+    return render(request, 'peselvalidator/dashboard.html', {
+        'form': form,
+        'result': result
+    })
+
